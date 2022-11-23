9e668c125d2a66a666f6a2c386a446c994e1fe4c
Allison Valdez <ajsnipes@usfca.edu>
2022-11-23 13:37:13 -0500

    second solution

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// Exercise 1
 function countdown(num) {
     //why do we need to intialize it to variable?
-    let number = setInterval(function () {
+    let number = setInterval(() => {
         num--;
 
         if (num <= 0) {
@@ -13,3 +14,24 @@ function countdown(num) {
     }, 1000);
 }
 console.log(countdown(4));
+
+/* 
+Both code executes simultaneously to run Exercise 2 uncomment out this line of code.
+
+
+function randomGame() {
+    let randonum;
+    let times = 0;
+    let timer = setInterval(() => {
+        console.log("hello");
+        randonum = Math.random();
+        times++;
+
+        if (randonum > .75) {
+            clearInterval(timer);
+            console.log("It took " +times + " to run.");
+        }
+    }, 1000);
+
+}
+console.log(randomGame()); */
